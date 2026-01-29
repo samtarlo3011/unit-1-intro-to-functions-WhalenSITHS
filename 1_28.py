94a95f45ebeddf9f24656c0367b567984dc84b64
samtarlo3011 <samuelt136@nycstudents.net>
2026-01-29 10:12:47 -0500

    Update 1_28.py

--- 1_28.py
+++ 1_28.py
@@ -1,6 +1,7 @@
 import turtle
 from turtle import *
 t = Turtle()
+t.speed(1000000)
 def square(x):
     t.forward(x)
     t.left(90)
@@ -9,6 +10,7 @@ def square(x):
     t.forward(x)
     t.left(90)
     t.forward(x)
+    t.left(90)
 
 
 """ for i in range(4):
@@ -18,6 +20,28 @@ def triangle(x):
         t.forward(x)
         t.left(120)
 """ triangle(50) """
+def star(x):
+    t.forward(x)
+    t.left(144)
+    t.forward(x)
+    t.left(144)
+    t.forward(x)
+    t.left(144)
+    t.forward(x)
+    t.left(144)
+    t.forward(x)
+    t.left(144)
 
+""" starlength = 50
+for i in range(60):
+    star(starlength)
+    t.right(5)
+    starlength = starlength+5
+ """
+length = 50
+for i in range(60):
+    square(length)
+    t.right(5)
+    length = length + 10
 
 turtle.done()
